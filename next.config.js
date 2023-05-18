09c954edbef5fa8b94c4c8970431ed41ddc2bf03
김성집 <jiby@wemakeprice.com>
2023-05-18 15:21:11 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache.js')
+
+// const isProduction = process.env.NODE_ENV === 'production'
+
+const config = {
+  // ...원래 next config 파일
+}
+
+const nextConfig = withPWA({
+  dest: 'public',
+  // disable: !isProduction,
+  runtimeCaching: [],
+  // runtimeCaching,
+})(config)
 
 module.exports = nextConfig
---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // auto import
+  importOrder: ['^@/**/(.*)$', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: false,
+}
